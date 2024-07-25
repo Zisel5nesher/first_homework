3c123296ad26af2b610db61e8470711064c20a93
Zisel Hershkowitz <zissywe@gmail.com>
2024-07-25 12:46:29 -0400

    first commit statred two files based on celsius homework

---
+++ c to f.js
@@ -0,0 +1,12 @@
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+console.log(celsiusToFahrenheit(38));
+
+function farenheitToCelsius(farenheit){
+    return (farenheit-32)/1.8;
+
+}
+
+console.log(farenheitToCelsius(100.4));
